81eee7b8f9f029c071437f0fc659e1fa067a52ca
Ansh Dube <anshdube10801@gmail.com>
2022-11-13 12:36:31 +0530

    first commit

---
+++ contracts/Contest.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.5.16;
+
+contract Contest {
+
+struct  Contestant  {
+    uint id;
+    string name;
+    uint voteCount;
+}
+mapping(uint => Contestant) public contestants;
+
+mapping (address => bool) public voters;
+
+uint public contestantsCount;
+constructor() public{
+     addContestant("BJP");
+    addContestant("CONGRESS");
+    addContestant("AAP");
+    addContestant("BSP");
+   
+
+}
+
+
+
+function addContestant(string memory _name) private {
+contestantsCount ++;
+
+
+
+contestants[contestantsCount] = Contestant(contestantsCount,_name,0);
+
+
+}
+function vote(uint _contestantId) public {
+  
+   require(!voters[msg.sender]);
+   require(_contestantId > 0 && _contestantId <= contestantsCount);
+   
+    contestants[_contestantId].voteCount ++;
+
+    voters[msg.sender] = true;
+
+}
+
+
+}
+
